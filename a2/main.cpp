755dcc5bf51e1f2a26e00deb01a5757c54e82c1c
AndrewPC <AndrewPC@Andrew.telus>
2015-11-12 20:11:05 -0800

    Initial

---
+++ a2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: AndrewPC
+ *
+ * Created on November 12, 2015, 8:08 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World!\n";
+    return 0;
+}
+
